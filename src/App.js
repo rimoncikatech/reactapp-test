ceaef7d3de4d96a54690346ff978b75ee493fc5e
admin <rimonroy.cse@gmail.com>
2024-02-08 00:40:25 +0600

    test commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         deploy pipeline
+         deploy pipeline2
         </p>
         <a
           className="App-link"
